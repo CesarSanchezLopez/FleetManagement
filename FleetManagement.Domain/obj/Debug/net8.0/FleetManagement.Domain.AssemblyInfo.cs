77cc21628a1f43d0d614f846f9c9674cda8a702c
Cesar Sanchez <cesaradolfosanchezlopez@gmail.com>
2026-03-15 11:14:35 -0500

    RELEVANT COMMIT MESSAGE

--- FleetManagement.Domain/obj/Debug/net8.0/FleetManagement.Domain.AssemblyInfo.cs
+++ FleetManagement.Domain/obj/Debug/net8.0/FleetManagement.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FleetManagement.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad8d651deec20dc8459a24347f4ecf20b3583a23")]
 [assembly: System.Reflection.AssemblyProductAttribute("FleetManagement.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FleetManagement.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
